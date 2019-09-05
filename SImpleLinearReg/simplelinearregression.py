157f3b3d6b6eb3ba4f2551c39571b6a0e8388882
tejaskochar <39263308+tejaskochar@users.noreply.github.com>
2019-09-05 15:06:47 +0530

    Add files via upload

---
+++ SImpleLinearReg/simplelinearregression.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 20:19:25 2019
+
+@author: tejas
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#prediction
+y_pred = regressor.predict(X_test)
+
+#plotting the original training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show
+
+#plotting the test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show
+
+#Comparing the test results with the predicted test results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, y_pred, color = 'blue')
+plt.title('Salary vs Experience (Test Set) Predicted')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show
